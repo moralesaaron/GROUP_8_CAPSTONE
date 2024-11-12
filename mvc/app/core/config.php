c0ef484d2b3b78026a0be10aef96666e4ffaae1c
moralesaaron <157785983+moralesaaron@users.noreply.github.com>
2024-11-12 19:52:25 +0800

    added mvc template file

---
+++ mvc/app/core/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('APP_NAME', 'BSIS 3F');
+
+define('ROOT', 'http://localhost/bsis3f_weba_24/mvc/public');
+
+
+define('DB_HOST','localhost');
+define('DB_NAME', 'mvc_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
