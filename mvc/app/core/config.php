5b94ad08a5449dffd899e0a36703e6aad55ca33e
moralesaaron <moralesaaronpogi@gmail.com>
2024-11-24 22:43:34 +0800

    changed config.php

--- mvc/app/core/config.php
+++ mvc/app/core/config.php
@@ -1,13 +1,14 @@
 <?php
 
-//define('APP_NAME', 'DormLynk');
+define('APP_NAME', 'DormLynk');
 
 define('ROOT', 'http://localhost/group_8_capstone/mvc/public');
 
 
+
 //  URGENT URGENT URGENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //FIX PATH
-//define('PATH', 'C:/xampp/htdocs/GROUP_8_CAPSTONE/comlab/app/views/');
+define('PATH', 'C:/xampp/htdocs/GROUP_8_CAPSTONE/mvc/app/views/');
 
 define('DB_HOST','localhost');
 define('DB_NAME', 'doli_db');
